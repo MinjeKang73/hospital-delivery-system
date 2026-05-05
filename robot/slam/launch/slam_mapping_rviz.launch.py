3d7553907f5adfeee5554a3d3829ed84b31d76a8
MinjeKang73 <kmj@tukorea.ac.kr>
2026-05-05 23:31:28 +0900

    [KMJ] feat: slam preparation

--- robot/slam/launch/slam_mapping_rviz.launch.py
+++ robot/slam/launch/slam_mapping_rviz.launch.py
@@ -1,17 +1,23 @@
-import os
-
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+import os
 
 
 def generate_launch_description():
-    pkg_share = get_package_share_directory('med_bot_slam')
+    pkg_share = get_package_share_directory('slam')
 
     use_sim_time = LaunchConfiguration('use_sim_time')
+    params_file = LaunchConfiguration('params_file')
     rviz_config = LaunchConfiguration('rviz_config')
+
+    default_params_file = os.path.join(
+        pkg_share,
+        'config',
+        'slam_mapper_params.yaml'
+    )
 
     default_rviz_config = os.path.join(
         pkg_share,
@@ -25,10 +31,23 @@ def generate_launch_description():
             default_value='false'
         ),
         DeclareLaunchArgument(
+            'params_file',
+            default_value=default_params_file
+        ),
+        DeclareLaunchArgument(
             'rviz_config',
             default_value=default_rviz_config
         ),
-
+        Node(
+            package='slam_toolbox',
+            executable='async_slam_toolbox_node',
+            name='slam_toolbox',
+            output='screen',
+            parameters=[
+                params_file,
+                {'use_sim_time': use_sim_time}
+            ]
+        ),
         Node(
             package='rviz2',
             executable='rviz2',
